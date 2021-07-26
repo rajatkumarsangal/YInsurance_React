bd9ea034e09e730514a87d991003b75d0af50e02
Rajat Kumar Sangal <rajatsangal1@hotmail.com>
2021-07-26 22:44:11 +0530

    git branch -M main

---
+++ src/EndUserComponents/LayoutComponents/Footer.jsx
@@ -0,0 +1,351 @@
+import React from 'react';
+
+function Footer(){
+return(
+<React.Fragment>
+<footer className="padding_bottom padding_top">
+    <div className="container-fluid">
+        <div className="row">
+            <div className="col-xs-6 col-sm-3 col-md-2" style={{'padding-right':'0px'}}>
+                <ul>
+                    <li>
+                        <a href="general-insurance/health/health-home.html" target="_self">
+                            <span id="ctl00_Footer1_lblftrHealth">Health Insurance</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="general-insurance/critical/critical_home.html" target="_self">
+                            <span id="ctl00_Footer1_lblftrCritical">Health Critical Illness</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="general-insurance/SeniorCitizen/online_senior_citizen_home1.html"
+                           target="_self">
+                            <span id="ctl00_Footer1_lblftrSeniorCitizen">Senior Citizen Insurance</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="personal-accident/online-personal-accident-home.html" target="_self">
+                            <span id="ctl00_Footer1_lblftrPA">Personal Accident</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="general-insurance/topup/TopUp-home.html" target="_self">
+                            <span id="ctl00_Footer1_lblTopup">Top Up</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="general-insurance/travel/travel-home.html" target="_self">
+                            <span id="ctl00_Footer1_lblftrTravel">Travel Insurance</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="general-insurance/motor/online-motor-home.html" target="_self">
+                            <span id="ctl00_Footer1_lblftrPrivateMotor">Motor Insurance</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="general-insurance/motor/online-motor-insurance-commercial-home.html"
+                           target="_self">
+                            <span id="ctl00_Footer1_lblftrCommercialVehicle">Commercial Vehicle</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="general-insurance/twoWheeler/online-twowheeler-home.html" target="_self">
+                            <span id="ctl00_Footer1_lblftrTwoWheeler">Two Wheeler</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" target="_self">
+                            <span id="ctl00_Footer1_lblLibility">Liability</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="pet-insurance/online-pet-insurance-home.html" target="_self">
+                            <span id="ctl00_Footer1_lblftrPet">Pet Insurance</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="business-insurance/online_business_insurance_home.html" target="_self">
+                            <span id="ctl00_Footer1_lblftrBusiness">Business Insurance</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="fine-art-insurance/online_fine_art_insurance_home.html" target="_self">
+                            <span id="ctl00_Footer1_lblftrFineArt">Fine Art  Insurance</span>
+                        </a>
+                    </li>
+                </ul>
+                {/* <!-- ul --> */}
+            </div>
+            {/* <!-- .col-md-3 --> */}
+            <div className="col-xs-6 col-sm-3 col-md-2">
+                <ul>
+                    <li>
+                        <a href="life-insurance/TermLife/online-term-insurance-home.html" target="_self">
+                            <span id="ctl00_Footer1_lblftrTerm">Term Plans</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="life-insurance/Child/online-child-insurance-home.html" target="_self">
+                            <span id="ctl00_Footer1_lblftrChild">Child Plans</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="life-insurance/Pension/online-retirement-insurance-home.html"
+                           target="_self">
+                            <span id="ctl00_Footer1_lblftrPension">Pension Plans</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="life-insurance/online-guaranteed-insurance-plan-home.html" target="_self">
+                            <span id="ctl00_Footer1_lblftrInvestment">Wealth Creation Plans</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="life-insurance/online-investment-insurance-home.html" target="_self">
+                            <span id="ctl00_Footer1_lblftrSaving">Investment Plan</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="UTI-ULIP/online-UTI-ULIP-home.html" target="_self">
+                            <span id="ctl00_Footer1_lblftrUTI">ULIP Plan</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="rural-insurance/Rural-insurance-homeb150.html?ProductId=weather" target="_self">
+                            <span id="ctl00_Footer1_lblftrRural">Rural Insurance</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="rural-insurance/Rural-insurance-homeb712.html?ProductId=rainfall"
+                           target="_self">
+                            <span id="ctl00_Footer1_lblftrRainfall">Rainfall Insurance</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="rural-insurance/Rural-insurance-home9b82.html?ProductId=crop" target="_self">
+                            <span id="ctl00_Footer1_lblftrCrop">Crop Insurance</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="rural-insurance/Rural-insurance-home254f.html?ProductId=plantation"
+                           target="_self">
+                            <span id="ctl00_Footer1_lblftrPlantation">Plantation</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="rural-insurance/farmer-package-policy-home.html" target="_self">
+                            <span id="ctl00_Footer1_lblftrFarmer">Farmer Package Policy</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="rural-insurance/pumpset-insurance-home.html" target="_self">
+                            <span id="ctl00_Footer1_lblftrPumpset">Pumpset</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="rural-insurance/cattle-insurance-home.html" target="_self">
+                            <span id="ctl00_Footer1_lblftrCattle">Cattle Insurance</span>
+                        </a>
+                    </li>
+                </ul>
+                {/* <!-- ul --> */}
+            </div>
+            {/* <!-- .col-md-3 --> */}
+            <div className="col-xs-6 col-sm-3 col-md-2">
+                <ul>
+                    <li>
+                        <a href="PortfolioManagement/Login.html" target="_self">
+                            <span id="ctl00_Footer1_lblManageyourPolicy">Manage Your Policy</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="Global/Blog.html" target="_self">
+                            <span id="ctl00_Footer1_lblftrIIBlog">InsuringIndia Blog</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="Global/FAQ.html" target="_self">
+                            <span id="ctl00_Footer1_lblftrFAQ">FAQs</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="Global/GlossaryOfTerms.html" target="_self">
+                            <span id="ctl00_Footer1_lblftrGlossary">Glossary of Terms</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="Global/BuyingGuide.html" target="_self">
+                            <span id="ctl00_Footer1_lblftrBuyingGuide">Buying Guide</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="Global/ContactUs.html" target="_self">
+                            <span id="ctl00_Footer1_lblftrContactUsQuery">Contact us for any query</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="canHelpTemplate0132.html?needHelp=true" target="_self">
+                            <span id="ctl00_Footer1_lblftrNeedHelp">Need help?</span>
+                        </a>
+                    </li>
+                </ul>
+               {/*  <!-- ul --> */}
+            </div>
+            {/* <!-- .col-md-3 --> */}
+            <div className="col-xs-6 col-sm-3 col-md-2">
+                <ul>
+                    <li>
+                        <a href="Global/AboutUs.html" target="_self">
+                            <span id="ctl00_Footer1_lblftrAboutUs">About Us</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="Global/ManagementTeam.html" target="_self">
+                            <span id="ctl00_Footer1_lblftrMngtTeam">Management Team</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="Global/WorkCulture.html" target="_self">
+                            <span id="ctl00_Footer1_lblftrWrkCulture">Work Culture</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="Global/WhyUs.html" target="_self">
+                            <span id="ctl00_Footer1_Label1">Why Us</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="Global/insuringindia-foundation1.html" target="_self">
+                            <span id="ctl00_Footer1_lblftrIIFoundation">InsuringIndia Foundation</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="Global/Careers1.html" target="_self">
+                            <span id="ctl00_Footer1_lblftrCareer">Careers </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="Global/current-openings1.html" target="_self">
+                            <span id="ctl00_Footer1_lblftrCurrentOpening">Current  Opening</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="Global/Terms-of-use.html" target="_self">
+                            <span id="ctl00_Footer1_lblftrAgreement">User Agreement</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="Global/Disclaimer.html" target="_self">
+                            <span id="ctl00_Footer1_lblftrDisclaimer">Disclaimer</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="Global/Privacy-Policy.html" target="_self">
+                            <span id="ctl00_Footer1_lblftrPrivacyPolicy">Privacy Policy</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="Global/ContactUs.html" target="_self">
+                            <span id="ctl00_Footer1_lblftrContactUs">Contact Us</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="SiteMap1.html" target="_self">
+                            <span id="ctl00_Footer1_lblftrSiteMap">Site Map</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="Global/Partners.html" target="_self">
+                            <span id="ctl00_Footer1_lblftrPartners">Partners</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="feedback.html" target="_self">
+                            <span id="ctl00_Footer1_lblFeedback">Feedback</span>
+                        </a>
+                    </li>
+                </ul>
+                {/* <!-- ul --> */}
+            </div>
+            {/* <!-- .col-md-3 --> */}
+            <div className="col-xs-12 col-md-4">
+                <div className="footer_social text-center">
+                    <a href="http://www.facebook.com/pages/Insuring-India/185072224855280" target="_blank">
+                        <div className="footer_image">
+                            <div className="border f_face">
+                            </div>
+                            <img alt="" className="footer_face" src="/images/footer_face.png" />
+                        </div>
+                        {/* <!-- .footer_image --> */}
+                    </a><a href="http://www.srstechsolution.com">
+                        <div className="footer_image">
+                            <div className="border f_twitt">
+                            </div>
+                            <img alt="" className="footer_face" src="/images/footer_twitt.png" />
+                        </div>
+                        {/* <!-- .footer_image --> */}
+                    </a><a href="Global/Blog.html">
+                        <div className="footer_image">
+                            <div className="border f_blogg">
+                            </div>
+                            <img alt="" className="footer_face" src="/images/footer_blogg.png" />
+                        </div>
+                        {/* <!-- .footer_image --> */}
+                    </a><a href="http://www.linkedin.com/company/1899506">
+                        <div className="footer_image">
+                            <div className="border f_link">
+                            </div>
+                            <img alt="" className="footer_face" src="/images/footer_link_icn.png" />
+                        </div>
+                        {/* <!-- .footer_image --> */}
+                    </a><a href="https://plus.google.com/u/0/b/117362241206591459905/+Insuringindia1/posts">
+                        <div className="footer_image">
+                            <div className="border f_google">
+                            </div>
+                            <img alt="" className="footer_face" src="/images/footer_google_plus.png" />
+                        </div>
+                        {/* <!-- .footer_image --> */}
+                    </a>
+                </div>
+                <div className="text-center">
+                    <img alt="" src="/images/master/QR_code.png" className="img-responsive scan_image" />
+                    <p className="copy_text">
+                        <span id="ctl00_Footer1_lblBarCode">Scan the QR code from your mobile device to access mobile version of insuringindia.com</span>
+                    </p>
+                    <p className="copy_text">
+                        <span id="ctl00_Footer1_lblftrInsuranceSolitication">Insurance is the subject matter of solicitation</span>
+                    </p>
+                </div>
+            </div>
+        </div>
+        {/* <!-- .row --> */}
+    {/* <!-- .container --> */}
+    <div className="container-fluid">
+        <div className="row">
+            <div className="col-md-12">
+
+                <p className="copy_text">
+                    <span id="ctl00_Footer1_FooterContent2">© 2011 InsuringIndia.com and Great Indian Insurance Web Aggregators Pvt. Ltd. <div className="clearfix"></div><p className="copy_text fontstyle2">Website Approved by the IRDA (Govt. of India)<br />Regn. No. IRDA/WBA/26/01/2015</p></span>
+                </p>
+                <p className="copy_text">
+                    <span id="ctl00_Footer1_lblFooterContent3">All the images used on InsuringIndia.com have either been purchased or are used with permission of the copyright holder. Images from InsuringIndia.com may not be copied or used elsewhere without obtaining  appropriate permissions from the copyright holders.<div className="clearfix"></div>All Rights reserved</span>
+                </p>
+
+            </div>
+            {/* <!-- .col-md-12 --> */}
+        </div>
+        {/* <!-- .row --> */}
+    </div>
+    {/* <!-- .container-fluid --> */}
+</div>
+
+
+</footer>
+{/* <!-- Footer close --> */}
+</React.Fragment>
+);
+}
+
+export default Footer;
